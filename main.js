22b5fc615d1131103e86f585c81e722bb07f150d
4shktz <evilancasteras@edenschool.fr>
2023-10-28 21:50:01 +0200

    Create project

---
+++ main.js
@@ -0,0 +1,30 @@
+function getTime() {
+    let t = 1000; 
+    setTimeout('showTime()', t)
+}
+
+function showTime() {
+    let date = new Date()
+    let heures = date.getHours();
+    let minutes = date.getMinutes();
+    let secondes = date.getSeconds();
+
+    if (heures < 10) {
+        heures = '0' + heures;
+    }
+
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+
+    if (secondes < 10) {
+        secondes = '0' + secondes;
+    }
+
+    // let time = heures + ':' + minutes + ':' + secondes;
+    let time = '#' + heures + minutes + secondes;
+
+    document.querySelector('#time').innerHTML = time;
+    document.body.style.backgroundColor = time;
+    getTime();
+}
